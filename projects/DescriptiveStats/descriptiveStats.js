6bd07c3bb59bc9083bf9484cc8fb132a547120f5
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-03-27 20:12:15 -0500

    fix mode, realtime updates

--- projects/DescriptiveStats/descriptiveStats.js
+++ projects/DescriptiveStats/descriptiveStats.js
@@ -2,25 +2,33 @@
 // By Ted Silbernagel
 
 // Declare variables so they're global
-let calcButton;
-let boxSummary, boxMean, boxMedian, boxMode, boxRange;
+let resultsTable, boxSummary, boxMean, boxMedian, boxMode, boxRange;
 let boxVariance, boxStDev, boxStErr, boxConfInt, boxConfLevel;
 let i;
 
-// Set up function to get data from user
-function getUserData() {
-  let userData = document.getElementById('userData').value;
-  let dataType = document.querySelector('input[name="dataType"]:checked').value;
-  let confLevel = document.querySelector('input[name="confLevel"]:checked').value;
-  return {userData: userData,
-          dataType: dataType,
-          confLevel: confLevel
-        }
+// Set up keyUp listener to auto calculate
+function checkLastKey() {
+  let entryBoxValue = document.getElementById('userData').value;
+
+  // First show the table
+  if (entryBoxValue.length === 0) {
+    resultsTable.style.display = 'none';
+  } else {
+    resultsTable.style.display = 'table';
+  }
+
+  const regexToTest = /[a-z||0-9]/i;
+  if (regexToTest.test(entryBoxValue.substr(-1))) {
+    runDescriptiveStats();
+  }
+}
+function setUpKeyUpListener() {
+  document.getElementById('userData').onkeyup = function() { checkLastKey(); };
 }
 
 // Set up variables to hold DOM elements
 function initialiseDomVariables() {
-  calcButton = document.getElementById('calcButton');
+  resultsTable = document.getElementById('resultsTable');
   boxSummary = document.getElementById('summary');
   boxMean = document.getElementById('mean');
   boxMedian = document.getElementById('median');
@@ -33,9 +41,15 @@ function initialiseDomVariables() {
   boxConfLevel = document.getElementById('confLevel');
 }
 
-// Set up onclick listener
-function setUpOnClick() {
-  calcButton.onclick = function(){ runDescriptiveStats(); };
+// Set up function to get data from user
+function getUserData() {
+  let userData = document.getElementById('userData').value;
+  let dataType = document.querySelector('input[name="dataType"]:checked').value;
+  let confLevel = document.querySelector('input[name="confLevel"]:checked').value;
+  return {userData: userData,
+          dataType: dataType,
+          confLevel: confLevel
+        }
 }
 
 // Set up function to run the process and display results
@@ -79,7 +93,7 @@ const descriptiveStats = (values) => {
   for (i in data) {
       sum += data[i];
   }
-  const mean = sum / dataLength;
+  let mean = sum / dataLength;
 
   // Calculate median
   let sortedData = data.sort();
@@ -93,34 +107,50 @@ const descriptiveStats = (values) => {
 
   // Calculate mode
   let modes = data;
+  let modeString = 'None';
   let loopedModeNos = [];
   let numOccurrences = [];
-  //let deleteRestModes = false;
+  // Get the number of occurrences per number
   for (i in modes) {
     if (loopedModeNos.indexOf(modes[i]) === -1) {
-      console.log('not found');
-      console.log(i);
-      console.log(modes[i]);
-      console.log(loopedModeNos.indexOf(modes[i]));
       loopedModeNos.push(modes[i]);
       numOccurrences.push(1);
     } else {
-      console.log('found');
-      console.log(i);
-      console.log(modes[i]);
-      console.log(loopedModeNos.indexOf(modes[i]));
       loopedModeNos.push(modes[i]);
       numOccurrences.push(0);
       numOccurrences[loopedModeNos.indexOf(modes[i])] += 1;
     }
   }
-  console.log(modes);
-  console.log(loopedModeNos);
-  console.log(numOccurrences);
-
-  for (i in modes) {
-    if (i[modes.indexOf(i)] !== max(numOccurrences)) {
-
+  // If no occurrences less than 0, set to empty array
+  let maxMode = 0;
+  for (i in numOccurrences) {
+    if (numOccurrences[i] > maxMode) {
+      maxMode = numOccurrences[i];
+    }
+  }
+  if (maxMode <= 1) {
+    modes = [];
+  } else {
+    // Remove all numbers with occurrences less than max
+    for (i = modes.length - 1; i >= 0; i--) {
+      if (numOccurrences[modes.indexOf(modes[i])] !== Math.max(...numOccurrences)) {
+        loopedModeNos.splice(i, 1);
+      }
+    }
+    // Remove duplicates
+    modes = [...new Set(loopedModeNos)];
+    // Put modes into string
+    modeString = '';
+    for (i in modes) {
+      if (modes.length == 1) {
+        modeString = modes[0];
+      } else if (i == 0) {
+        modeString += '[' + modes[i];
+      } else if (i == modes.length - 1) {
+        modeString += ', ' + modes[i] + ']';
+      } else {
+        modeString += ', ' + modes[i];
+      }
     }
   }
 
@@ -130,7 +160,7 @@ const descriptiveStats = (values) => {
   const max = sortedData[lastData];
   range.push(min, max);
 
-  // Calculate variance and standard deviation
+  // Calculate variance
   let sqDiffFromMean = [];
   let sqDifference = 0;
   for (i in data) {
@@ -141,6 +171,8 @@ const descriptiveStats = (values) => {
   for (i in sqDiffFromMean) {
     variance += sqDiffFromMean[i];
   }
+
+  // Calculate standard deviation
   variance /= dof;
   let stDev = Math.sqrt(variance);
 
@@ -152,6 +184,7 @@ const descriptiveStats = (values) => {
   let upperBound = mean + (tStat * stErr);
 
   // Round numbers
+  mean = mean.toFixed(roundTo);
   variance = variance.toFixed(roundTo);
   stDev = stDev.toFixed(roundTo);
   stErr = stErr.toFixed(roundTo);
@@ -164,7 +197,7 @@ const descriptiveStats = (values) => {
           dof: dof,
           mean: mean,
           median: median,
-          mode: modes,
+          mode: modeString,
           min: min,
           max: max,
           variance: variance,
@@ -178,7 +211,7 @@ const descriptiveStats = (values) => {
 
 // Set up function to print results
 const printResults = (results) => {
-  boxSummary.innerHTML = `${results.dataType} of ${results.n} observations`
+  boxSummary.innerHTML = `<b>${results.dataType} of ${results.n} observations</b>`
   boxMean.innerHTML = `${results.mean}`
   boxMedian.innerHTML = `${results.median}`
   boxMode.innerHTML = `${results.mode}`
@@ -186,24 +219,14 @@ const printResults = (results) => {
   boxVariance.innerHTML =`${results.variance}`
   boxStDev.innerHTML = `${results.stDev}`
   boxStErr.innerHTML = `${results.stErr}`
-  boxConfInt.innerHTML = `${results.confLevel}% Confidence Level:`
+  boxConfInt.innerHTML = `${results.confLevel}% Confidence Interval:`
   boxConfLevel.innerHTML = `[${results.lowerBound}, ${results.upperBound}]`
 };
-
-/*
-
-boxMedian
-boxMode
-boxRange
-boxVariance
-boxStDev
-boxStErr
-boxConfInt
-*/
-
 
 // Start script once DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   initialiseDomVariables();
-  setUpOnClick();
+  setUpKeyUpListener();
+  // Hide results, initially
+  resultsTable.style.display = 'none';
 })
