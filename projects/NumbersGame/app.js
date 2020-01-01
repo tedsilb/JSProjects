e0a45b98fb8f90159321d3f223dd4f57e1ff3098
tedsilb <26172399+tedsilb@users.noreply.github.com>
2020-01-01 00:27:25 -0600

    add autofocus, bugfixes, more comments

--- projects/NumbersGame/app.js
+++ projects/NumbersGame/app.js
@@ -23,23 +23,37 @@ const setHighAndLow = () => {
 
 // Save target number (step 1)
 const saveTargetNumber = () => {
+  // Get input, validate
   targetNo = parseInt(targetNumberInput.value);
   targetNumberInput.value = '';
   if (notOk(targetNo)) return;
+
+  // If ok, hide current row, show next
   targetRow.style.display = 'none';
   upperLimitRow.style.display = 'block';
+
+  // Set focus to next input
+  upperLimitInput.focus();
 }
 
 // Save upper limit (step 2)
 const saveUpperLimit = () => {
+  // Get input, validate
   upperNo = parseInt(upperLimitInput.value);
   upperLimitInput.value = '';
   if (notOk(upperNo)) return;
+
+  // If ok, hide current row, show next
   upperLimitRow.style.display = 'none';
   guessNumberRow.style.display = 'block';
   highLowRow.style.display = 'block';
+
+  // Reset previous field, update upper/lower limits
   resultText.innerHTML = '';
   setHighAndLow();
+
+  // Set focus to next input
+  guessNumberInput.focus();
 }
 
 // Handle new guess (step 3+)
@@ -68,7 +82,6 @@ const handleNewGuess = () => {
     guessNumberRow.style.display = 'none';
     startGameButton.innerHTML = 'Start new game';
     startGameButton.classList.add('button-primary');
-    return;
   }
   guessNumberInput.value = '';
 }
@@ -93,6 +106,9 @@ const resetGame = () => {
   guessNumberRow.style.display = 'none';
   highLowRow.style.display = 'none';
 
+  // Set focus to target number input
+  targetNumberInput.focus();
+
   // Reset target, upper, and lower numbers
   targetNo = 0;
   upperNo = 0;
@@ -101,16 +117,25 @@ const resetGame = () => {
 
 // Set up variables to hold DOM elements
 const initialiseDomVariables = () => {
+  // Start game button
   startGameButton = document.getElementById('startGameButton');
+
+  // Target number
   targetRow = document.getElementById('targetRow');
   targetNumberInput = document.getElementById('targetNumber');
   saveTargetNumberButton = document.getElementById('saveTargetNumberButton');
+
+  // Upper limit
   upperLimitRow = document.getElementById('upperLimitRow');
   upperLimitInput = document.getElementById('upperLimit');
   saveUpperLimitButton = document.getElementById('saveUpperLimitButton');
+
+  // Guess number
   guessNumberRow = document.getElementById('guessNumberRow');
   guessNumberInput = document.getElementById('guessNumber');
   saveguessNumberButton = document.getElementById('saveguessNumberButton');
+
+  // Results display
   highLowRow = document.getElementById('highLowRow');
   resultText = document.getElementById('resultText');
   highResultText = document.getElementById('highResultText');
@@ -125,9 +150,32 @@ const setUpButtonListeners = () => {
   saveguessNumberButton.onclick = () => { handleNewGuess(); };
 }
 
+// Set up enter key handlers for inputs
+const setUpEnterKeyHandlers = () => {
+  targetNumberInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {  // 13 == enter key
+      event.preventDefault();  // Cancel the default action, if needed
+      saveTargetNumberButton.click();
+    }
+  });
+  upperLimitInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {  // 13 == enter key
+      event.preventDefault();  // Cancel the default action, if needed
+      saveUpperLimitButton.click();
+    }
+  });
+  guessNumberInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {  // 13 == enter key
+      event.preventDefault();  // Cancel the default action, if needed
+      saveguessNumberButton.click();
+    }
+  });
+}
+
 // Start script once DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   initialiseDomVariables();
   setUpButtonListeners();
+  setUpEnterKeyHandlers();
   hideMainRows();
 })
